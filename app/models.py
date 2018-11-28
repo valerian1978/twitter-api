9bbc21ed8c1e5d97cd85c782420d26fee3101662
zx12r4@yahoo.fr <zx12r4@yahoo.fr>
2018-11-28 16:37:44 +0100

    update

---
+++ app/models.py
@@ -0,0 +1,8 @@
+# app/models.py
+from datetime import datetime
+
+class Tweet:
+    def __init__(self, text):
+        self.id = None
+        self.text = text
+        self.created_at = datetime.now()
---
+++ app/repositories.py
@@ -0,0 +1,19 @@
+# app/repositories.py
+class TweetRepository:
+    def __init__(self):
+        self.__clear()
+
+    def add(self, tweet):
+        self.tweets.append(tweet)
+        tweet.id = self.next_id
+        self.next_id += 1
+
+    def get(self, id):
+      for tweet in self.tweets:
+          if tweet.id == id:
+              return tweet
+      return None
+
+    def __clear(self):
+      self.tweets = []
+      self.next_id = 1
